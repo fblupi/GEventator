4614dc2be3c7a3203745f61e8cd5949bf81f1843
Francisco Javier Bolívar Lupiáñez <fblupi@correo.ugr.es>
2016-11-23 00:03:35 +0100

    easy_install_package en Chef closes #17

--- provision/Chef/chef/cookbooks/geventator/recipes/default.rb
+++ provision/Chef/chef/cookbooks/geventator/recipes/default.rb
@@ -5,10 +5,5 @@ package 'python-setuptools'
 package 'python-dev'
 package 'python-pip'
 
-execute 'Flask' do
-  command 'pip install Flask'
-end
-
-execute 'Flask-PyMongo' do
-  command 'pip install Flask-PyMongo'
-end
+easy_install_package 'Flask'
+easy_install_package 'Flask-PyMongo'
